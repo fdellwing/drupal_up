f76329aee93f24fd3a53151f95ebfb04301ea4d3
Fabian Dellwing <f.dellwing@netufutura.de>
2019-06-03 13:37:58 +0200

    Refactor duplicate code Allow more configs Improve codestyle

--- drupal-up.sh
+++ drupal-up.sh
@@ -2,7 +2,7 @@
 ######################################################
 ###   Script for (semi)automatic Drupal Updates    ###
 ###      Author: https://github.com/fdellwing      ###
-###                Date: 01.12.2016                ###
+###                Date: 03.06.2019                ###
 ###        Contact: f.dellwing@netfutura.de        ###
 ######################################################
 
@@ -13,7 +13,14 @@
 
 # The root path for your drupal installations
 WWW_PATH="/var/www/"
+# Database backup directory
 DB_BACKUP_PATH="/root/drupal_update_db_back/"
+# Log file directory
+LOG_PATH="/var/log/"
+# Drupal files owner (33=www-data)
+OID=33
+# Drupal files group (33=www-data)
+GID=33
 
 # Your systems MYSQL user settings
 # If you do not use a debian based system,
@@ -24,6 +31,38 @@ DB_BACKUP_PATH="/root/drupal_update_db_back/"
 # password = passphrase
 # socket   = /var/run/mysqld/mysqld.sock
 CONF="/etc/mysql/debian.cnf"
+
+######################################################
+###     Important functions used by the script     ###
+######################################################
+
+function set_maintenance {
+	if [ "$D_VERSION" -eq 7 ]; then
+		# Set maintenance mode
+		drush @sites vset maintenance_mode 1 -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log
+		# Clear the cache to make sure we are in maintenance
+		drush @sites cc all -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log # clear cache
+	else
+		# Set maintenance mode
+		drush @sites sset system.maintenance_mode 1 -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log
+		# Clear the cache to make sure we are in maintenance
+		drush @sites cr -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log # clear cache
+	fi
+}
+
+function unset_maintenance {
+	if [ "$D_VERSION" -eq 7 ]; then
+		# Unset maintenance mode
+		drush @sites vset maintenance_mode 0 -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log
+		# Clear the cache to make sure we are not in maintenance
+		drush @sites cc all -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log # clear cache
+	else
+		# Unset maintenance mode
+		drush @sites sset system.maintenance_mode 0 -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log
+		# Clear the cache to make sure we are not in maintenance
+		drush @sites cr -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log # clear cache
+	fi
+}
 
 ######################################################
 ###          The main script starts here.          ###
@@ -38,47 +77,43 @@ elif [ -z "$2" ]; then
 	# Delete old db backups
 	find "$DB_BACKUP_PATH" -iname "*" -mtime +90 -delete 2> /dev/null || mkdir -p "$DB_BACKUP_PATH"
 	# Clear the logfiles from previous run
-	date >| /var/log/drupal-mysql.log
-	date >| /var/log/drupal-up.log
+	if [ ! -d "$LOG_PATH" ]; then
+		mkdir -p "$LOG_PATH"
+	fi
+	date >| "$LOG_PATH"drupal-mysql.log
+	date >| "$LOG_PATH"drupal-up.log
 	# Check if the given parameter is a directory in WWW_PATH
 	if [ -d "$WWW_PATH""$1" ]; then
-		LIST=false
+		drupale=( "$1" )
 	else
 		# If not, is it a file?
 		if [ -e "$1" ]; then
 			# Creates an array from the input file
-			IFS=$'\n' drupale=( $( cat "$1" ) )
-			LIST=true
+			drupale=()
+			while IFS=$'\n' read -r line; do drupale+=("$line"); done < <(cat "$1")
 		else
-		# If not, exit the script
+			# If not, exit the script
 			echo "----------------------"
 			echo 'The given parameter is no existing directory or file.'
 			echo "----------------------"
 			exit 1
 		fi
 	fi
-	# Run the routine for a single folder
-	if [ "$LIST" = false ]; then
+	echo "----------------------"
+	echo 'Starting update for '"${#drupale[@]}"' instances.'
+	echo "----------------------"
+	for drupal in "${drupale[@]}"
+		do
 		# Get the databases from the drupal settings
-		IFS=$'\n' datenbanken=( $( grep -R -h -E "^[[:space:]]*'database' => '" "$WWW_PATH""$1"/sites/*/settings.php ) )
-		TMP_PATH="$WWW_PATH""$1"
+		datenbanken=()
+		while IFS=$'\n' read -r line; do datenbanken+=("$line"); done < <(grep -R -h -E "^[[:space:]]*'database' => '" "$WWW_PATH""$drupal"/sites/*/settings.php | grep -Po "(?<==> ').*(?=')")
+		TMP_PATH="$WWW_PATH""$drupal"
 		cd "$TMP_PATH" || exit 1
-		# Check for Drupal Version
 		D_VERSION=$(drush @sites status -y --format=json 2> /dev/null | grep 'drupal-version' | grep -Eo '[0-9]+\.' | head -c 1)
 		echo "----------------------"
-		echo 'Starting update for '"$1"'.'
+		echo 'Starting update for '"$drupal"'.'
 		echo "----------------------"
-		if [ "$D_VERSION" -eq 7 ]; then
-			# Set maintenance mode
-			drush @sites vset maintenance_mode 1 -y >> /dev/null 2>> /var/log/drupal-up.log
-			# Clear the cache to make sure we are in maintenance
-			drush @sites cc all -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-		else
-			# Set maintenance mode
-                        drush @sites sset system.maintenance_mode 1 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        # Clear the cache to make sure we are in maintenance
-                        drush @sites cr -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-		fi
+		set_maintenance
 		echo "----------------------"
 		echo 'Site(s) moved to maintenance mode.'
 		echo "----------------------"
@@ -90,34 +125,17 @@ elif [ -z "$2" ]; then
 		# Create the DB backups
 		for db in "${datenbanken[@]}"
 			do
-			# Get that variable in the right form
-			db=${db#*\'}
-			db=${db#*\'}
-			db=${db#*\'}
-			db=${db%%\'*}
 			# Dump the database in in self contained file
-			mysqldump --defaults-extra-file="$CONF" --add-drop-table "$db" | gzip > "$DB_BACKUP_PATH""$db""_""$(date +'%Y_%m_%d')".sql.gz 2>> /var/log/drupal-mysql.log # sql file erstellen
-			# shellcheck disable=SC2181
 			# If the command fails, we need to stop or we can harm our drupal permanently
-			if [ "$?" -eq 0 ]; then
+			if mysqldump --defaults-extra-file="$CONF" --add-drop-table "$db" | gzip > "$DB_BACKUP_PATH""$db""_""$(date +'%Y_%m_%d')".sql.gz 2>> "$LOG_PATH"drupal-mysql.log; then
 				echo "----------------------"
 				echo 'Database backup successfully created ('"$i"'/'"${#datenbanken[@]}"').'
 				echo "----------------------"
 			else
 				echo "----------------------"
-				echo 'Error while creating the database backup, please check the logfile "/var/log/drupal-mysql.log".'
+				echo "Error while creating the database backup, please check the logfile \"$LOG_PATH""drupal-mysql.log\"."
 				echo "----------------------"
-                		if [ "$D_VERSION" -eq 7 ]; then
-                        		# Unset maintenance mode
-                        		drush @sites vset maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        		# Clear the cache to make sure we are not in maintenance
-                        		drush @sites cc all -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                		else
-                        		# Unset maintenance mode
-                        		drush @sites sset system.maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        		# Clear the cache to make sure we are not in maintenance
-                        		drush @sites cr -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                		fi
+				unset_maintenance
 				# If you are here, please read the log, because there is something wrong
 				exit 1
 			fi
@@ -127,158 +145,35 @@ elif [ -z "$2" ]; then
 		echo 'Starting update of drupal.'
 		echo "----------------------"
 		# Do the drupal update
-		drush @sites rf -y >> /dev/null 2>> /var/log/drupal-up.log
-		drush @sites up -y >> /dev/null 2>> /var/log/drupal-up.log
+		drush @sites rf -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log
+		drush @sites up -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log
 		echo "----------------------"
 		echo 'Finishing update of drupal.'
 		echo "----------------------"
 		# To be sure, do a DB update
-		drush @sites updatedb -y >> /dev/null 2>> /var/log/drupal-up.log
-		# Set the correct owner (33=www-data)
-		chown -R 33:33 "$TMP_PATH"
-                if [ "$D_VERSION" -eq 7 ]; then
-                	# Unset maintenance mode
-                	drush @sites vset maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        # Clear the cache to make sure we are not in maintenance
-                        drush @sites cc all -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                else
-                        # Unset maintenance mode
-                        drush @sites sset system.maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        # Clear the cache to make sure we are not in maintenance
-                        drush @sites cr -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                fi
-		# Clear error log from previous run
-		date >| /var/log/drupal-up-error.log
-		# Put all the errors from the log to the error log
-		grep error /var/log/drupal-up.log >> /var/log/drupal-up-error.log
-		# Count the lines in error log >1 = there are errors
-		LINECOUNT=$(wc -l /var/log/drupal-up-error.log)
-		LINECOUNT=${LINECOUNT%% *}
-		if [ "$LINECOUNT" -gt 1 ]; then
-			echo "----------------------"
-			echo 'Site(s) moved out of maintenance mode, please check the website(s).'
-			echo 'There are some errors, please check the logfile "/var/log/drupal-up-error.log"'
-			echo "----------------------"
-		else
-			echo "----------------------"
-			echo 'Site(s) moved out of maintenance mode, please check the website(s).'
-			echo "----------------------"
-		fi
-	# Run the routine for multiple drupal installations
+		drush @sites updatedb -y >> /dev/null 2>> "$LOG_PATH"drupal-up.log
+		# Set the correct owner
+		chown -R $OID:$GID "$TMP_PATH"
+		unset_maintenance
+		echo "----------------------"
+		echo 'Site(s) moved out of maintenance mode, please check the website(s).'
+		echo "----------------------"
+	done
+	# Clear error log from previous run
+	date >| "$LOG_PATH"drupal-up-error.log
+	# Put all the errors from the log to the error log
+	grep error "$LOG_PATH"drupal-up.log >> "$LOG_PATH"drupal-up-error.log
+	# Count the lines in error log >1 = there are errors
+	LINECOUNT=$(wc -l "$LOG_PATH"drupal-up-error.log | cut -f1 -d' ')
+	if [ "$LINECOUNT" -gt 1 ]; then
+		echo "----------------------"
+		echo 'All updates finished.'
+		echo "There are some errors, please check the logfile \"$LOG_PATH""drupal-up-error.log\"."
+		echo "----------------------"
 	else
 		echo "----------------------"
-		echo 'Starting update for '"${#drupale[@]}"' entries.'
+		echo 'All updates finished.'
 		echo "----------------------"
-		for drupal in "${drupale[@]}"
-			do
-			# Get the databases from the drupal settings
-			IFS=$'\n' datenbanken=( $( grep -R -h -E "^[[:space:]]*'database' => '" "$WWW_PATH""$drupal"/sites/*/settings.php ) )
-			TMP_PATH="$WWW_PATH""$drupal"
-			cd "$TMP_PATH" || exit 1
-			D_VERSION=$(drush @sites status -y --format=json 2> /dev/null | grep 'drupal-version' | grep -Eo '[0-9]+\.' | head -c 1)
-			echo "----------------------"
-			echo 'Starting update for '"$drupal"'.'
-			echo "----------------------"
-                	if [ "$D_VERSION" -eq 7 ]; then
-                        	# Set maintenance mode
-                        	drush @sites vset maintenance_mode 1 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        	# Clear the cache to make sure we are in maintenance
-                        	drush @sites cc all -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                	else
-                        	# Set maintenance mode
-                        	drush @sites sset system.maintenance_mode 1 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        	# Clear the cache to make sure we are in maintenance
-                        	drush @sites cr -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                	fi
-			echo "----------------------"
-			echo 'Site(s) moved to maintenance mode.'
-			echo "----------------------"
-			echo "----------------------"
-			echo 'Starting '"${#datenbanken[@]}"' database backup(s).'
-			echo "----------------------"
-			# shellcheck disable=SC2034
-			i=1
-			# Create the DB backups
-			for db in "${datenbanken[@]}"
-				do
-				# Get that variable in the right form
-				db=${db#*\'}
-				db=${db#*\'}
-				db=${db#*\'}
-				db=${db%%\'*} # db name
-				# Dump the database in in self contained file
-				mysqldump --defaults-extra-file="$CONF" --add-drop-table "$db" | gzip > "$DB_BACKUP_PATH""$db""_""$(date +'%Y_%m_%d')".sql.gz 2>> /var/log/drupal-mysql.log
-				# shellcheck disable=SC2181
-				# If the command fails, we need to stop or we can harm our drupal permanently
-				if [ "$?" -eq 0 ]; then
-					echo "----------------------"
-					echo 'Database backup successfully created ('"$i"'/'"${#datenbanken[@]}"').'
-					echo "----------------------"
-				else
-					echo "----------------------"
-					echo 'Error while creating the database backup, please check the logfile "/var/log/drupal-mysql.log".'
-					echo "----------------------"
-					if [ "$D_VERSION" -eq 7 ]; then
-                  			      	# Unset maintenance mode
-                        			drush @sites vset maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        			# Clear the cache to make sure we are not in maintenance
-                        			drush @sites cc all -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                			else
-                        			# Unset maintenance mode
-                        			drush @sites sset system.maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        			# Clear the cache to make sure we are not in maintenance
-                        			drush @sites cr -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                			fi
-					# If you are here, please read the log, because there is something wrong
-					exit 1
-				fi
-				((i++))
-			done
-			echo "----------------------"
-			echo 'Starting update of drupal.'
-			echo "----------------------"
-			# Do the drupal update
-			drush @sites rf -y >> /dev/null 2>> /var/log/drupal-up.log
-			drush @sites up -y >> /dev/null 2>> /var/log/drupal-up.log
-			echo "----------------------"
-			echo 'Finishing update of drupal.'
-			echo "----------------------"
-			# To be sure, do a DB update
-			drush @sites updatedb -y >> /dev/null 2>> /var/log/drupal-up.log
-			# Set the correct owner (33=www-data)
-			chown -R 33:33 "$TMP_PATH"
-			if [ "$D_VERSION" -eq 7 ]; then
-                        	# Unset maintenance mode
-                        	drush @sites vset maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        	# Clear the cache to make sure we are not in maintenance
-                        	drush @sites cc all -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                	else
-                        	# Unset maintenance mode
-                        	drush @sites sset system.maintenance_mode 0 -y >> /dev/null 2>> /var/log/drupal-up.log
-                        	# Clear the cache to make sure we are not in maintenance
-                        	drush @sites cr -y >> /dev/null 2>> /var/log/drupal-up.log # cache clearen
-                	fi
-			echo "----------------------"
-			echo 'Site(s) moved out of maintenance mode, please check the website(s).'
-			echo "----------------------"
-		done
-		# Clear error log from previous run
-		date >| /var/log/drupal-up-error.log
-		# Put all the errors from the log to the error log
-		grep error /var/log/drupal-up.log >> /var/log/drupal-up-error.log
-		# Count the lines in error log >1 = there are errors
-		LINECOUNT=$(wc -l /var/log/drupal-up-error.log) # fehler zählen
-		LINECOUNT=${LINECOUNT%% *}
-		if [ "$LINECOUNT" -gt 1 ]; then
-			echo "----------------------"
-			echo 'All updates finished.'
-			echo 'There are some errors, please check the logfile "/var/log/drupal-up-error.log"'
-			echo "----------------------"
-		else
-			echo "----------------------"
-			echo 'All updates finished.'
-			echo "----------------------"
-		fi
 	fi
 # Display usage if more than one parameter is given
 else
